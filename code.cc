9af8c12fe921707c31b7c46ff36913b0ae7fd856
red2fred2 <admin@red2fred2.com>
2018-01-22 22:05:09 -0600

    randomly smashed in lcd example

--- code.cc
+++ code.cc
@@ -1,5 +1,6 @@
 #pragma platform(VEX2)
 #pragma competitionControl(Competition)
+#pragma config(UART_Usage, UART2, uartVEXLCD, baudRate19200, IOPins, None, None)
 #include "Vex_Competition_Includes.c"
 
 /****************************************************************
@@ -494,4 +495,177 @@ task usercontrol() {
     armControl();
     baseLiftControl();
   }
+}
+
+
+
+
+
+
+
+const int lcdLeft = 1;
+const int lcdCenter = 2;
+const int lcdRight = 4;
+ 
+//Wait for Press--------------------------------------------------
+void waitForPress()
+{
+while(nLCDButtons == 0){}
+wait1Msec(5);
+}
+//----------------------------------------------------------------
+ 
+//Wait for Release------------------------------------------------
+void waitForRelease()
+{
+while(nLCDButtons != 0){}
+wait1Msec(5);
+}
+//----------------------------------------------------------------
+ 
+task main()
+{
+//Declare count variable to keep track of our choice
+int count = 0;
+ 
+//------------- Beginning of User Interface Code ---------------
+//Clear LCD
+clearLCDLine(0);
+clearLCDLine(1);
+//Loop while center button is not pressed
+while(nLCDButtons != lcdCenter)
+{
+//Switch case that allows the user to choose from 4 different options
+switch(count){
+case 0:
+//Display first choice
+displayLCDCenteredString(0, "Autonomous 1");
+displayLCDCenteredString(1, "<         Enter        >");
+waitForPress();
+//Increment or decrement "count" based on button press
+if(nLCDButtons == lcdLeft)
+{
+waitForRelease();
+count = 3;
+}
+else if(nLCDButtons == ldeRight)
+{
+waitForRelease();
+count++;
+}
+break;
+case 1:
+//Display second choice
+displayLCDCenteredString(0, "Autonomous 2");
+displayLCDCenteredString(1, "<         Enter        >");
+waitForPress();
+//Increment or decrement "count" based on button press
+if(nLCDButtons == lcdLeft)
+{
+waitForRelease();
+count--;
+}
+else if(nLCDButtons == lcdRight)
+{
+waitForRelease();
+count++;
+}
+break;
+case 2:
+//Display third choice
+displayLCDCenteredString(0, "Autonomous 3");
+displayLCDCenteredString(1, "<         Enter        >");
+waitForPress();
+//Increment or decrement "count" based on button press
+if(nLCDButtons == lcdLeft)
+{
+waitForRelease();
+count--;
+}
+else if(nLCDButtons == lcdRight)
+{
+waitForRelease();
+count++;
+}
+break;
+case 3:
+//Display fourth choice
+displayLCDCenteredString(0, "Autonomous 4");
+displayLCDCenteredString(1, "<         Enter        >");
+waitForPress();
+//Increment or decrement "count" based on button press
+if(nLCDButtons == lcdLeft)
+{
+waitForRelease();
+count--;
+}
+else if(nLCDButtons == lcdRight)
+{
+waitForRelease();
+count = 0;
+}
+break;
+default:
+count = 0;
+break;
+}
+}
+//------------- End of User Interface Code ---------------------
+ 
+//------------- Beginning of Robot Movement Code ---------------
+//Clear LCD
+clearLCDLine(0);
+clearLCDLine(1);
+//Switch Case that actually runs the user choice
+switch(count){
+case 0:
+//If count = 0, run the code correspoinding with choice 1
+displayLCDCenteredString(0, "Autonomous 1");
+displayLCDCenteredString(1, "is running!");
+wait1Msec(2000);                        // Robot waits for 2000 milliseconds
+ 
+// Move forward at full power for 3 seconds
+motor[rightMotor] = 127;            // Motor on port2 is run at full (127) power forward
+motor[leftMotor]    = 127;            // Motor on port3 is run at full (127) power forward
+wait1Msec(3000);                            // Robot runs previous code for 3000 milliseconds before moving on
+break;
+case 1:
+//If count = 1, run the code correspoinding with choice 2
+displayLCDCenteredString(0, "Autonomous 2");
+displayLCDCenteredString(1, "is running!");
+wait1Msec(2000);                        // Robot waits for 2000 milliseconds
+ 
+// Move reverse at full power for 3 seconds
+motor[rightMotor] = -127;            // Motor on port2 is run at full (-127) power reverse
+motor[leftMotor]    = -127;            // Motor on port3 is run at full (-127) power reverse
+wait1Msec(3000);                            // Robot runs previous code for 3000 milliseconds before moving on
+break;
+case 2:
+//If count = 2, run the code correspoinding with choice 3
+displayLCDCenteredString(0, "Autonomous 3");
+displayLCDCenteredString(1, "is running!");
+wait1Msec(2000);                        // Robot waits for 2000 milliseconds
+ 
+//Turn right for 3seconds
+motor[rightMotor] = -63;            // Motor on port2 is run at half power reverse
+motor[leftMotor]    = 63;                // Motor on port3 is run at half power forward
+wait1Msec(3000);                            // Robot runs previous code for 3000 milliseconds before moving on
+break;
+case 3:
+//If count = 3, run the code correspoinding with choice 4
+displayLCDCenteredString(0, "Autonomous 4");
+displayLCDCenteredString(1, "is running!");
+wait1Msec(2000);                        // Robot waits for 2000 milliseconds
+ 
+//Turn left for 3 seconds
+motor[rightMotor] = 63;                // Motor on port2 is run at half power forward
+motor[leftMotor]    = -63;            // Motor on port3 is run at half power reverse
+wait1Msec(3000);                            // Robot runs previous code for 3000 milliseconds before moving on
+break;
+default:
+displayLCDCenteredString(0, "No valid choice");
+displayLCDCenteredString(1, "was made!");
+break;
+}
+//------------- End of Robot Movement Code -----------------------
 }
