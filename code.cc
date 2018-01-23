98b04db2714138a639a9325219cf3944285afa67
red2fred2 <admin@red2fred2.com>
2018-01-22 19:28:30 -0600

    auto driveStraight

--- code.cc
+++ code.cc
@@ -7,43 +7,57 @@
 ****************************************************************/
 
 //drivetrain
-const int leftDriveDeadzone = 10;
-const double leftDriveMult = 1.0;
-const int rightDriveDeadzone = 10;
-const double rightDriveMult = 1.0;
+const int leftDriveDeadzone               = 10;
+const double leftDriveMult                = 1.0;
+const int rightDriveDeadzone              = 10;
+const double rightDriveMult               = 1.0;
 
-const double drivePMult = 1.0;
-const double driveIMult = 1.0;
-const double driveDMult = 1.0;
+const double leftPMult                    = 1.0;
+const double leftIMult                    = 1.0;
+const double leftDMult                    = 1.0;
+
+const double rightPMult                   = 1.0;
+const double rightIMult                   = 1.0;
+const double rightDMult                   = 1.0;
+
+const double inchesPerWheelRotation       = 1.0;
+const double leftEncoderTicksPerRotation  = 100.0;
+const double rightEncoderTicksPerRotation = 100.0;
+const int leftTicksPerInch = (int)(leftEncoderTicksPerRotation / inchesPerWheelRotation);
+const int rightTicksPerInch = (int)(rightEncoderTicksPerRotation / inchesPerWheelRotation);
+
 
 ////////////////////////////////////////////////////////////////
 //lift
-const int leftLiftDeadzone = 10;
-const double leftLiftMult = 1.0;
+const int leftLiftDeadzone  = 10;
+const double leftLiftMult   = 1.0;
 const int rightLiftDeadzone = 10;
-const double rightLiftMult = 1.0;
+const double rightLiftMult  = 1.0;
 
 /////////////////////////////////////////////////////////////////
 
 //arm
 const int armDeadzone = 10;
-const double armMult = 1.0;
+const double armMult  = 1.0;
 
 /////////////////////////////////////////////////////////////////
 
 //baseLift
-const int leftBaseLiftDeadzone = 10;
-const double leftBaseLiftMult = 1.0;
+const int leftBaseLiftDeadzone  = 10;
+const double leftBaseLiftMult   = 1.0;
 const int rightBaseLiftDeadzone = 10;
-const double rightBaseLiftMult = 1.0;
+const double rightBaseLiftMult  = 1.0;
 
 
 /****************************************************************
 ////////////////////////// Variables ////////////////////////////
 ****************************************************************/
 
-double driveIntegral = 0;
-double driveError = 0;
+double leftIntegral = 0;
+double leftError = 0;
+double rightIntegral = 0;
+double rightError = 0;
+
 bool tankMode = false;
 bool buttonLock = false;
 
@@ -133,48 +147,81 @@ void nullifyRightDriveEncoder() {
 ///////////////////////// PID Functions /////////////////////////
 ****************************************************************/
 
-double p(double target, double actual, double Pmult) {
+double P(double target, double actual, double Pmult) {
   return Pmult * (target - actual);
 }
 
 /////////////////////////////////////////////////////////////////
 
-double i(double target, double actual, double Pmult, double Imult) {
-  driveI += p(target, actual, Pmult);
-  return Imult * driveIntegral;
+double leftI(double target, double actual, double Pmult, double Imult) {
+  leftIntegral += P(target, actual, Pmult);
+  return Imult * leftIntegral;
+}
+double rightI(double target, double actual, double Pmult, double Imult) {
+  rightIntegral += P(target, actual, Pmult);
+  return Imult * rightIntegral;
 }
 
 /////////////////////////////////////////////////////////////////
 
-double d(double target, double actual, double Pmult, double Dmult) {
-  double newError = p(target, actual, Pmult);
-  double diff = newError - driveError;
-  driveError = newError;
+double leftD(double target, double actual, double Pmult, double Dmult) {
+  double newError = P(target, actual, Pmult);
+  double diff = newError - leftError;
+  leftError = newError;
+  return Dmult * newError;
+}
+double rightD(double target, double actual, double Pmult, double Dmult) {
+  double newError = P(target, actual, Pmult);
+  double diff = newError - rightError;
+  rightError = newError;
   return Dmult * newError;
 }
 
 /////////////////////////////////////////////////////////////////
 
-double pi(double target, double actual, double Pmult, double Imult) {
-  return p(target, actual, Pmult) + i(target, actual, Imult);
+int leftPI(double target, double actual, double Pmult, double Imult) {
+  return (int)(P(target, actual, Pmult) + leftI(target, actual, Imult));
+}
+int rightPI(double target, double actual, double Pmult, double Imult) {
+  return (int)(P(target, actual, Pmult) + rightI(target, actual, Imult));
 }
 
 /////////////////////////////////////////////////////////////////
 
-double pd(double target, double actual, double Pmult, double Dmult) {
-  return p(target, actual, Pmult) + d(target, actual, Dmult);
+int leftPD(double target, double actual, double Pmult, double Dmult) {
+  return (int)(P(target, actual, Pmult) + leftD(target, actual, Dmult));
+}
+int rightPD(double target, double actual, double Pmult, double Dmult) {
+  return (int)(P(target, actual, Pmult) + leftD(target, actual, Dmult));
 }
 
 /////////////////////////////////////////////////////////////////
 
-double id(double target, double actual, double Imult, double Dmult) {
-  return i(target, actual, Imult) + d(target, actual, Dmult);
+int leftID(double target, double actual, double Imult, double Dmult) {
+  return (int)(leftI(target, actual, Imult) + leftD(target, actual, Dmult));
+}
+int rightID(double target, double actual, double Imult, double Dmult) {
+  return (int)(rightI(target, actual, Imult) + rightD(target, actual, Dmult));
 }
 
 /////////////////////////////////////////////////////////////////
 
-double pid(double target, double actual, double Pmult, double Imult, double Dmult) {
-  return p(target, actual, Pmult) + i(target, actual, Imult) + d(target, actual, Dmult);
+int leftPID(double target, double actual, double Pmult, double Imult, double Dmult) {
+  return (int)(P(target, actual, Pmult) + leftI(target, actual, Imult) + leftD(target, actual, Dmult));
+}
+int rightPID(double target, double actual, double Pmult, double Imult, double Dmult) {
+  return (int)(P(target, actual, Pmult) + rightI(target, actual, Imult) + rightD(target, actual, Dmult));
+}
+
+/////////////////////////////////////////////////////////////////
+
+void resetLeftPID() {
+  leftIntegral = 0;
+  leftError = 0;
+}
+void resetRightPID() {
+  rightIntegral = 0;
+  rightError = 0;
 }
 
 
@@ -221,6 +268,19 @@ void baseLiftControl() {
 /****************************************************************
 //////////////////////// Auto Functions /////////////////////////
 ****************************************************************/
+
+void driveStraight(double inches) {
+  resetLeftPID();
+  resetRightPID();
+
+  int leftTicks = leftTicksPerInch * inches + getLeftDriveEncoder;
+  int rightTicks = rightTicksPerInch * inches + getRightDriveEncoder;
+
+  drive(
+    leftPID(leftTicks, getLeftDriveEncoder, leftPMult, leftIMult, leftDMult),
+    rightPID(rightTicks, getRightDriveEncoder, rightPMult, rightIMult, rightDMult)
+  );
+}
 
 
 /****************************************************************
